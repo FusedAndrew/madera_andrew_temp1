62220595747e2de51e65caed30a7b756b62dae3b
FusedAndrew <72750640+FusedAndrew@users.noreply.github.com>
2020-11-19 13:23:29 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+tempFi = input.temperature(TemperatureUnit.FAHRENHEIT)
+tempF = 80
+#while True:
+    #print("Temperature (F): " + input.temperature(TemperatureUnit.FAHRENHEIT) + " - Temperature (C): " + input.temperature(TemperatureUnit.CELSIUS))
+    #print("Current Room Temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT) + "°F - " + input.temperature(TemperatureUnit.CELSIUS) + "°C")
+
+
+#if tempF > 60:
+    #light.set_pixel_color(5, light.rgb(0, 255, 0))
+
+if tempFi > 90:
+    light.set_pixel_color(5, light.rgb(255, 0, 0))
+elif tempFi > 40:
+    light.set_pixel_color(5, light.rgb(0, 255, 0))
+else:
+    light.set_pixel_color(5, light.rgb(0, 0, 255))
+
+#if tempF > 70:
+    #light.set_all(light.rgb(255,0,0))
+#elif tempF > 40:
+    #light.set_all(light.rgb(0,255,0))
+#else:
+    #ight.set_all(light.rgb(0,0,255))
 
--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
+let tempFi = input.temperature(TemperatureUnit.Fahrenheit)
+let tempF = 80
+// while True:
+// print("Temperature (F): " + input.temperature(TemperatureUnit.FAHRENHEIT) + " - Temperature (C): " + input.temperature(TemperatureUnit.CELSIUS))
+// print("Current Room Temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT) + "°F - " + input.temperature(TemperatureUnit.CELSIUS) + "°C")
+// if tempF > 60:
+// light.set_pixel_color(5, light.rgb(0, 255, 0))
+if (tempFi > 90) {
+    light.setPixelColor(5, light.rgb(255, 0, 0))
+} else if (tempFi > 40) {
+    light.setPixelColor(5, light.rgb(0, 255, 0))
+} else {
+    light.setPixelColor(5, light.rgb(0, 0, 255))
+}
 
